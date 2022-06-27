6d25191f38f99c309bf1effd55a6614bcb0024dc
aeayerdis <108297740+aeayerdis@users.noreply.github.com>
2022-06-27 16:01:17 -0400

    Update ns1auth.py

--- ns1cli-scripts/ns1auth.py
+++ ns1cli-scripts/ns1auth.py
@@ -1,12 +1,19 @@
+##Create pvar.json (POST Key) and gvar.json (ReadOnly Key) and save to your user home directory or wherever you want
+#[
+#{
+#  "apiKey":"keyID"  
+#}
+#]
+
 import json
 
-with open('C:/Users/Aaron.Ayerdis/pvar.json', 'r') as auth:
+with open('C:/Users/UserName/pvar.json', 'r') as auth:
     p_data = json.load(auth)
 
 for p in p_data:
     pkey = p["apiKey"]
 
-with open('C:/Users/Aaron.Ayerdis/gvar.json', 'r') as authg:
+with open('C:/Users/UserName/gvar.json', 'r') as authg:
     g_data = json.load(authg)
 
 for g in g_data:
